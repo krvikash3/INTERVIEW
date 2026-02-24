e12deed014162cb5f7c6104a6e96eb582a90ecb1
vikash <krvikash0912@gmail.com>
2026-02-24 14:12:37 +0530

    add pointer example with memory-related outputs

--- pointers/pointer.cpp
+++ pointers/pointer.cpp
@@ -5,7 +5,21 @@ int main()
 {
     // address of var->>
     int a;
+    cin >> a;
     cout << &a << endl;
+    //  create pointer
+    int *ptr; // integer type pointer
+    ptr = &a; // assign address of a
+    //*p that means value at p->
+    cout << *ptr << endl;
+    cout << sizeof(ptr) << endl;
+
+    // Memory based questions->
+    cout << a << endl;    // i think value of a
+    cout << &a << endl;   // address of a
+    cout << ptr << endl;  // address of a
+    cout << *ptr << endl; // value at p
+    cout << &ptr << endl; // address of p
 
     return 0;
 }
