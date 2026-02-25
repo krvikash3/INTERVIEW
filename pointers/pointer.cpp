d16f6d340bf22efe8202a36375ce21cf94cb9307
vikash <krvikash0912@gmail.com>
2026-02-25 09:17:38 +0530

    add more tricky question

--- pointers/pointer.cpp
+++ pointers/pointer.cpp
@@ -13,6 +13,7 @@ int main()
     //*p that means value at p->
     cout << *ptr << endl;
     cout << sizeof(ptr) << endl;
+    cout << a << endl;
 
     // Memory based questions->
     cout << a << endl;    // i think value of a
@@ -20,6 +21,13 @@ int main()
     cout << ptr << endl;  // address of a
     cout << *ptr << endl; // value at p
     cout << &ptr << endl; // address of p
+
+    *ptr = *ptr + 2; // this is true
+    cout << *ptr << endl;
+    cout << a << endl;
+
+    ptr = ptr + 1;
+    cout << ptr;
 
     return 0;
 }
